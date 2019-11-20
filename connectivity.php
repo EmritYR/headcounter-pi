2f3e16fecb741ea48d92ea56f418650ab4e24224
EmritYR <emrit.ramharracksingh@my.uwi.edu>
2019-11-20 17:37:14 -0400

    header for index

--- connectivity.php
+++ connectivity.php
@@ -1,5 +1,5 @@
 <?php
-function SignIn()
+function login()
 {
     session_start();
 
@@ -7,17 +7,16 @@ function SignIn()
     $password = hash('sha256', $_POST['pass']);
 
     try {
-        $myPDO = new PDO('pgsql:host=ec2-54-225-106-93.compute-1.amazonaws.com; port=5432; dbname=dcgqbc3e7j5gh0 sslmode=require', 'bczntonffplpss', '6e8a86add2b8b97e354e343b49e3dab4990ad633d2adcc1f85c1635a2ebf1dbe');
-        $result = $myPDO->query("SELECT * FROM users");
+        $myPDO = new PDO('pgsql:ec2-54-221-195-148.compute-1.amazonaws.com; port=5432; dbname=d3bfq4clh09b21 sslmode=require', 'qqolorykjuhkzg', 'aaf3efea7997f8b655d1b34dcffd6c3c5664eafdc4fb58591adef8df6b780a15');
+        $result = $myPDO->query("SELECT * FROM lecturer");
 
         while ($row = $result->fetch(\PDO::FETCH_ASSOC)) {
-            if ($row['username'] == $username and $row['password'] == $password) {
+            if ($row['lecturer_id'] == $username and $row['password_hash'] == $password) {
                 echo "found user";
                 $_SESSION['username'] = $row['password'];
-                $_SESSION['empid'] = $row['employee_id'];
-                $_SESSION['user'] = $row['username'];
-                $_SESSION['fname'] = $row['fname'];
-                $_SESSION['lname'] = $row['lname'];
+                $_SESSION['lecturer_id'] = $row['lecturer_id'];
+                $_SESSION['user'] = $row['name'];
+                $_SESSION['img_url'] = $row['img_url'];
                 echo 'logged in';
             }
         }
@@ -25,7 +24,7 @@ function SignIn()
         if (empty($_SESSION['username'])) {
             header("Location: login.php");
         } else {
-            if ($_SESSION['fname'] == 'admin')
+            if ($_SESSION['user'] == 'admin')
                 header("Location: admin_index.php");
             else
                 header("Location: home.php");
@@ -35,9 +34,8 @@ function SignIn()
     }
 }
 
-function Logout() {
+function logout() {
     session_start();
-
     unset($_SESSION["username"]);
     session_destroy();
     header("Location: index.php");
