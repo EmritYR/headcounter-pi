94f6ffe8a12ff7003b9a4b1b9b34ee253b12e00a
EmritYR <emrit.ramharracksingh@my.uwi.edu>
2019-11-23 02:59:31 -0400

    [UPDATE] Merge

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Headcounter</title>
+    <title>HeadCounter</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
