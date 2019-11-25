1cb3cb2aae06914533ca121ab1b5fd35e39493a5
EmritYR <emrit.ramharracksingh@my.uwi.edu>
2019-11-25 01:59:45 -0400

    [UPDATE] update test

--- pi_controller.php
+++ pi_controller.php
@@ -1,22 +1,10 @@
 <?php
-function getPDO()
-{
-    try {
-        $myPDO = new PDO('pgsql:host=ec2-54-221-195-148.compute-1.amazonaws.com; port=5432; dbname=d3bfq4clh09b21 sslmode=require', 'qqolorykjuhkzg', 'aaf3efea7997f8b655d1b34dcffd6c3c5664eafdc4fb58591adef8df6b780a15');
-        return $myPDO;
-    } catch (PDOException $e) {
-        echo "Failed: " . $e->getMessage();
-        return null;
-    }
-}
 function main() {
     if (isset($_POST['start_scanning'])) {
         exec('python3 scripts/startScanning.py');
-        echo "Start Scanning";
     }
     if (isset($_POST['stop_scanning'])) {
         exec('sudo -u root -S touch stop_scanning 2>&1');
-        echo "Stop Scanning";
     }
 }
 
--- update.php
+++ update.php
@@ -3,6 +3,6 @@ session_start();
 if($_SESSION['user'] != 'admin')
     header('Location: index.php');
 
-$output = exec('git pull https://StallionsProject:qwerty.12stallions@github.com/EmritYR/headcounter-pi');
+$output = exec('sudo -u root -S git pull https://StallionsProject:qwerty.12stallions@github.com/EmritYR/headcounter-pi 2>&1');
 echo "<pre>$output</pre>";
 ?>
