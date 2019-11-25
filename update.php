ad5b921a8a4161a763d6e6b9028047073b0af50b
EmritYR <emrit.ramharracksingh@my.uwi.edu>
2019-11-24 22:42:01 -0400

    [UPDATE] update update

--- update.php
+++ update.php
@@ -3,6 +3,6 @@ session_start();
 if($_SESSION['user'] != 'admin')
     header('Location: index.php');
 
-$output = shell_exec('git pull https://StallionsProject:qwerty.12stallions@github.com/EmritYR/headcounter-pi');
+$output = exec('git pull https://StallionsProject:qwerty.12stallions@github.com/EmritYR/headcounter-pi');
 echo "<pre>$output</pre>";
 ?>
